32808c4cf797cdbc60e50ccf9c78b52f20aad624
Lim Nan <2601652@sit.singaporetech.edu.sg>
2026-09-04 17:35:50 +0800

    Activity 4

--- hello.py
+++ hello.py
@@ -19,3 +19,16 @@ print("Day 2", followers)
 
 followers -=10
 print("Day 3", followers)
+
+username = input("Enter username: ")
+age = int(input("Enter age: "))
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("===================")
+print("Username:", username)
+print("Age:", age)
+print("Content Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you?")
