d6798eb7b85e5647a9b6b78e51a09c266723345e
Lim Nan <2601652@sit.singaporetech.edu.sg>
2026-09-04 17:25:28 +0800

    Activity 2

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("================================")
 print("Welcome here")
 print("My First post!")
 print("================================")
+
+username = "Cool_creator"
+bio ="Fun blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
